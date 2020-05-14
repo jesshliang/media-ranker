871196e27e08afe87d485f733ab408b59f881b0b
Jessica Liang <jessicursliang@gmail.com>
2020-05-13 17:58:27 -0700

    show page, link index to show

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -5,4 +5,13 @@ class WorksController < ApplicationController
 		@books = Work.where(category: 'book')
 	end
 
+	def show
+		@work = Work.find_by(id: params[:id])
+
+		if @work.nil?
+			head :not_found
+			return
+		end
+	end
+
 end
