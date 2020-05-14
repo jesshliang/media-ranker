43a08d6b9f412539d0f927fd2ed98cca2538448e
Jessica Liang <jessicursliang@gmail.com>
2020-05-13 22:44:31 -0700

    add randomly selected medias to index

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,13 @@
 class Work < ApplicationRecord
 
+	def self.top_books
+		books = Work.where(category: "book")
+		return books.sample(10)
+	end
+
+	def self.top_albums
+		albums = Work.where(category: "album")
+		return albums.sample(10)
+	end
+
 end
