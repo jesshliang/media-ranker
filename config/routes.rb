c4926aac7590a2b1cf66fcf09cb766252adaf682
Jessica Liang <jessicursliang@gmail.com>
2020-05-14 21:33:30 -0700

    setup for User + routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  get '/login', to: 'users#login_form', as: 'login'
+  post '/login', to: 'users#login'
+  post '/logout', to: 'users#logout', as: 'logout'
+
+  get "/users/current", to: 'users#current', as: 'current_user'
+
+  # get 'users/login_form'
+  # get 'users/login'
   root to: 'homepages#index'
 
   resources :works
