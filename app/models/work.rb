1b2cc2d2a985be7794da21d42cedab8eb33b1176
Jessica Liang <jessicursliang@gmail.com>
2020-05-16 00:07:25 -0700

    vote controller

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,6 @@
 class Work < ApplicationRecord
 	has_many :votes
-	has_many :users, through :votes
+	has_many :users, through: :votes
 
 	validates :category, presence: true
 	validates :title, presence: true
