6deb2a6e0c544253fe9cf6b539b7228ebfa90d10
Jessica Liang <jessicursliang@gmail.com>
2020-05-17 15:28:56 -0700

    cannot vote lest logged in

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,18 +1,27 @@
 class VotesController < ApplicationController
 
 	def create
-		@vote = Vote.new(
-			user_id: session[:user_id],
-			work_id: params[:work_id]
-		)
 
-		if @vote.save
-			flash[:success] = 'Upvoted!'
-			redirect_to works_path
-			return
+		if session[:user_id]
+
+			@vote = Vote.new(
+				user_id: session[:user_id],
+				work_id: params[:work_id]
+			)
+
+			if @vote.save
+				flash[:success] = 'Upvoted!'
+				redirect_to works_path
+				return
+			else
+				flash.now[:error] = 'Error; could not be upvoted.'
+				redirect_to works_path
+				return
+			end
+
 		else
-			flash.now[:error] = 'Error; could not be upvoted.'
-			redirect_to works_path
+			flash[:unauthorized] = 'You cannot vote unless you are logged in.'
+			redirect_to root_path
 			return
 		end
 	end
