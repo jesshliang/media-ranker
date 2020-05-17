6a8221f41cc1470ea4f93b4ead36adba3e41328f
Jessica Liang <jessicursliang@gmail.com>
2020-05-17 15:20:33 -0700

    add route, add link to upvote on show, show votes on page

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,8 +1,24 @@
 class VotesController < ApplicationController
 
 	def create
+		@vote = Vote.new(
+			user_id: session[:user_id],
+			work_id: vote_params[:work_id]
+		)
 
+		if @vote.save
+			flash[:success] = 'Upvoted!'
+			redirect_to works_path
+			return
+		else
+			flash.now[:error] = 'Error; could not be upvoted.'
+			redirect_to works_path
+			return
+		end
+	end
 
+	def vote_params
+		return params.require(:vote).permit(:work_id)
 	end
 	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
 
+  root to: 'homepages#index'
+
   get '/login', to: 'users#login_form', as: 'login'
   post '/login', to: 'users#login'
   post '/logout', to: 'users#logout', as: 'logout'
 
   get "/users/current", to: 'users#current', as: 'current_user'
 
-  root to: 'homepages#index'
-
-  resources :works
+  resources :works do
+    resources :votes, only: [:create]
+  end
+  
+  resources :votes, only: [:create]
 
 end
