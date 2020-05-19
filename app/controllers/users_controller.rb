4c1882f7dda3afcb63cbb0e598390c39105ec39b
Jessica Liang <jessicursliang@gmail.com>
2020-05-19 14:37:51 -0700

    css changes; add all users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+
   def login_form
     @user = User.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
     resources :votes, only: [:create]
   end
 
+  resources :users, only: [:index]
 end
