2e76a671bb290694b215ff2629575137126e91ec
Jessica Liang <jessicursliang@gmail.com>
2020-05-21 15:50:55 -0700

    add filter for showing user pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
 
-	def index
+	def current_user
+		@current_user = User.find_by(id: session[:user_id])
+	end
+
+	def require_login
+		if current_user.nil?
+			flash[:error] = "You must be logged in."
+			redirect_to login_path
+		end
 	end
 	
 end
